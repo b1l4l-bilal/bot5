1cc5adb980be33b0ff818974bc3b51ce1df4cf3d
b1l4l <b9500133@gmail.com>
2026-09-07 16:39:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,26 @@
-import os
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
-from dotenv import load_dotenv
-load_dotenv()
+import os
 
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from dotenv import load_dotenv
+
+load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
 @dp.message(CommandStart())
-async def start(message: types.Message):
-    await message.answer(
-        f"hiiiii"
-    )
+async def start_handler(message: Message):
+    await message.answer("Сәлем! 👋")
 
 
 async def main():
+    bot = Bot(token=TOKEN)
     await dp.start_polling(bot)
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
